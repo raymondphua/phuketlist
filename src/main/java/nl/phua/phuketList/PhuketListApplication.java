834de7811024aca1aba43e0e6cea2de597c1972c
raymondphua <chr.phua@student.avans.nl>
2017-11-10 01:29:04 +0100

    initial commit

---
+++ src/main/java/nl/phua/phuketList/PhuketListApplication.java
@@ -0,0 +1,12 @@
+package nl.phua.phuketList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhuketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhuketListApplication.class, args);
+	}
+}
